91e451890803741c070d5ef6d1c7a90cddc3289e
AndreRRodrigues <83727384+AndreRibeiroRodrigues@users.noreply.github.com>
2025-09-29 00:31:27 -0300

    pagina de apresentação criada com um link para a pagina inicial e funcional apresentando a contagem de valores nas tabelas

--- application/database.py
+++ application/database.py
@@ -7,7 +7,8 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
-#Chame a funcão get_db_connection() para obter uma conexão com o banco de dados SQLite.
+
+# Chame a funcão get_db_connection() para obter uma conexão com o banco de dados SQLite.
 # Exemplo de uso:
 # conn = get_db_connection()
 # cursor = conn.cursor()
@@ -26,5 +27,25 @@ def init_db():
     cursor.executescript(schema)
     conn.commit()
     conn.close()
+
+
 # A função init_db() inicializa o banco de dados executando um script SQL a partir de um arquivo chamado schema.sql.
 # Certifique-se de ter um arquivo schema.sql no mesmo diretório com a estrutura do banco
+
+def get_contagem_tabelas():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT COUNT(*) FROM ALUNO;")
+    alunos = cursor.fetchone()[0]
+
+    cursor.execute("SELECT COUNT(*) FROM EMPRESTIMO;")
+    emprestimos = cursor.fetchone()[0]
+
+    cursor.execute("SELECT COUNT(*) FROM LIVRO;")
+    livros = cursor.fetchone()[0]
+    print(alunos, emprestimos, livros)
+    return alunos, emprestimos, livros
+    # Exemplo de uso:
+    # init_db()
+    conn.close()
--- application/routes.py
+++ application/routes.py
@@ -1,12 +1,15 @@
 from flask import Blueprint, render_template
+import application.database as database
 import sqlite3
 
 bp = Blueprint('main', __name__)
 
 
 @bp.route('/')
+@bp.route('/apresentacao')
 def index():
-    return render_template('index.html')
+    alunos, emprestimos, livros = database.get_contagem_tabelas()
+    return render_template('index.html', alunos=alunos, emprestimos=emprestimos, livros=livros)
 
 
 @bp.route('/paginaInicial')
